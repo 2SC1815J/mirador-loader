af01bfa06e031af15c4e833e8bbbcc57ebc07c07
2SC1815J <2SC1815J@users.noreply.github.com>
2022-07-01 01:00:00 +0900

    update to the latest Mirador (v3.3.0 as of June 2022)

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Mirador Loader
 
-A Simple loader for the [Mirador](http://projectmirador.org/), accepts query parameters `manifest` and `canvas`.
+A Simple loader for the [Mirador](https://projectmirador.org/) that accepts query parameters `manifest` and `canvas`.
+
+You can pass multiple `manifest{n}` and `canvas{n}` query parameters (e.g., ?manifest=…&canvas=…&manifest1=…&canvas1=…&manifest2=…&canvas2=…).
+
+## Example
+
+http://2sc1815j.net/mirador-loader/?manifest=https://www.dl.ndl.go.jp/api/iiif/1287964/manifest.json&canvas=https://www.dl.ndl.go.jp/api/iiif/1287964/canvas/8&manifest1=http://codh.rois.ac.jp/pmjt/book/200024817/manifest.json&canvas1=http://codh.rois.ac.jp/pmjt/iiif/200024817/canvas/00065
--- index.js
+++ index.js
@@ -7,118 +7,31 @@
 (function() {
     'use strict';
 
-    processParams();
-
-    function processParams() {
-        var documentsInfo = [];
-        var canvases = [];
-        var result;
-        var reg = new RegExp(/(?:&|\?)canvas([0-9]+)?=(.+?)(?=&|$)/g);
-        while ((result = reg.exec(location.search)) !== null) {
-            if (result[2]) {
-                canvases[parseInt(result[1] || 0, 10)] = decodeURIComponent(result[2]);
-            }
-        }
-        reg = new RegExp(/(?:&|\?)manifest([0-9]+)?=(.+?)(?=&|$)/g);
-        while ((result = reg.exec(location.search)) !== null) {
-            if (result[2]) {
-                var index = parseInt(result[1] || 0, 10);
-                documentsInfo.push({
-                    index: index,
-                    manifestUri: decodeURIComponent(result[2]),
-                    canvasUri: canvases[index]
-                });
-            }
-        }
-        documentsInfo.sort(function(a, b) {
-            return a.index - b.index;
-        });
-        if (canvases.length > 0) {
-            getManifests(documentsInfo);
-        } else {
-            prepareMirador(documentsInfo);
+    var windowsInfo = [];
+    var canvases = [];
+    var result;
+    var reg = new RegExp(/(?:&|\?)canvas([0-9]+)?=(.+?)(?=&|$)/g);
+    while ((result = reg.exec(location.search)) !== null) {
+        if (result[2]) {
+            canvases[parseInt(result[1] || 0, 10)] = decodeURIComponent(result[2]);
         }
     }
-
-    function getManifests(documentsInfo) {
-        if (documentsInfo.length > 0) {
-            var deferreds = [];
-            for (var i = 0; i < documentsInfo.length; i++) {
-                deferreds.push($.getJSON(documentsInfo[i].manifestUri));
-            }
-            $.when.apply($, deferreds).done(function() {
-                var manifests = [];
-                if (deferreds.length === 1 && arguments[1] === 'success') {
-                    manifests.push(arguments[0]);
-                } else {
-                    for (i = 0; i < deferreds.length; i++) {
-                        if (arguments[i][1] === 'success') {
-                            manifests.push(arguments[i][0]);
-                        }
-                    }
-                }
-                if (documentsInfo.length === manifests.length) {
-                    prepareMirador(documentsInfo, manifests);
-                } else {
-                    prepareMirador(documentsInfo);
-                }
-            }).fail(function() {
-                prepareMirador(documentsInfo);
+    reg = new RegExp(/(?:&|\?)manifest([0-9]+)?=(.+?)(?=&|$)/g);
+    while ((result = reg.exec(location.search)) !== null) {
+        if (result[2]) {
+            var index = parseInt(result[1] || 0, 10);
+            windowsInfo.push({
+                index: index,
+                manifestId: decodeURIComponent(result[2]),
+                canvasId: canvases[index]
             });
-        } else {
-            prepareMirador(documentsInfo);
         }
     }
-
-    function prepareMirador(documentsInfo, manifests) {
-        var data = [];
-        var windowObjects = [];
-        for (var i = 0; i < documentsInfo.length; i++) {
-            data.push({
-                manifestUri: documentsInfo[i].manifestUri,
-                location: ''
-            });
-            var windowObject = {
-                loadedManifest: documentsInfo[i].manifestUri,
-                viewType: 'ImageView',
-                sidePanelVisible: false
-            };
-            if (documentsInfo[i].canvasUri && manifests) {
-                var canvasUri_ = documentsInfo[i].canvasUri.replace(/^https:/, 'http:');
-                try {
-                    for (var j = 0; j < manifests[i].sequences[0].canvases.length; j++) {
-                        if (manifests[i].sequences[0].canvases[j]['@id'].replace(/^https:/, 'http:') === canvasUri_) {
-                            windowObject.canvasID = documentsInfo[i].canvasUri;
-                            break;
-                        }
-                    }
-                } catch (e) {
-                    //
-                }
-            }
-            windowObjects.push(windowObject);
-        }
-        showMirador(data, windowObjects);
-    }
-
-    function showMirador(data_, windowObjects_) {
-        var data = data_ || [];
-        var windowObjects = windowObjects_ || [];
-        Mirador({
-            id: 'viewer',
-            layout: '1x' + (windowObjects.length || 1),
-            buildPath: 'http://projectmirador.org/demo/',
-            data: data,
-            windowObjects: windowObjects,
-            annotationEndpoint: {
-                name: 'Local Storage',
-                module: 'LocalStorageEndpoint'
-            }
-        });
-        var match = location.search.match(/(?:&|\?)title=(.+?)(?:&|$)/);
-        if (match) {
-            var title = decodeURIComponent(match[1]);
-            $('title').text(title + ' - Miradow Viewer');
-        }
-    }
+    windowsInfo.sort(function(a, b) {
+        return a.index - b.index;
+    });
+    Mirador.viewer({
+        id: 'mirador',
+        windows: windowsInfo,
+    });
 })();
